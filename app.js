826c3a1941dc3aea3f70e176060ba5346bccf430
FelixWaweru <wawerufelix60@gmail.com>
2023-04-02 16:26:35 +0300

    feat: Added response waiting

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 var chat = require('./gpt.js');
 var vocaliser = require('./voice.js');
 var player = require('play-sound')(opts = {})
-const { WebcastPushConnection } = require('tiktok-live-connector');
+const {
+    WebcastPushConnection
+} = require('tiktok-live-connector');
 var Sentiment = require('sentiment');
 require('dotenv').config();
 
 var sentiment = new Sentiment();
+var respondingTo = '';
 
-async function liveStream(){
+async function liveStream() {
     // Username of someone who is currently live
     let tiktokUsername = process.env.TIKTOK_USERNAME;
+    let speaking = false;
 
     // Create a new wrapper object and pass the username
     let tiktokLiveConnection = new WebcastPushConnection(tiktokUsername);
@@ -21,39 +25,69 @@ async function liveStream(){
         console.error('Failed to connect', err);
     });
 
-    // In this case we listen to chat messages (comments)
-    tiktokLiveConnection.on('chat', data => {
-        console.log(`${data.uniqueId} (userId:${data.userId}) writes: ${data.comment}`);
-        // Generate response
-        responseGenerator(data.comment, 'chat');
-    });
+    if (!speaking) {
+        // In this case we listen to chat messages (comments)
+        tiktokLiveConnection.on('chat', data => {
+            // Wait for response
+            speaking = true;
 
-    tiktokLiveConnection.on('gift', data => {
-        if (data.giftType === 1 && data.repeatEnd) {
-            console.log(`${data.uniqueId} (userId:${data.nickname}) gifted: ${data.repeatCount} x ${data.giftName}`);
-            // Streak ended or non-streakable gift => process the gift with final repeat_count
+            console.log(`${data.uniqueId} (userId:${data.userId}) writes: ${data.comment}`);
             // Generate response
-            const statement = `${data.nickname} by name for gifting ${data.repeatCount} ${data.giftName}`
-            responseGenerator(statement, 'gift');
-        }
-    })
+            respondingTo = data.nickname;
+            responseGenerator(data.comment, 'chat').then(response => {
+                // Complete response
+                speaking = false;
+            });
+        });
 
-    tiktokLiveConnection.on('follow', (data) => {
-        console.log(`${data.uniqueId} (userId:${data.nickname}) followed`);
-        // Generate response
-        const statement = `${data.nickname} by name`
-        responseGenerator(statement, 'follow');
-    });
+        tiktokLiveConnection.on('gift', data => {
+            // Wait for response
+            speaking = true;
 
-    tiktokLiveConnection.on('emote', data => {
-        console.log(`${data.uniqueId} (userId:${data.nickname}) emoted`);
-        // Generate response
-        const statement = `${data.nickname} by name`
-        responseGenerator(statement, 'emote');
-    });
+            if (data.giftType === 1 && data.repeatEnd) {
+                console.log(`${data.uniqueId} (userId:${data.nickname}) gifted: ${data.repeatCount} x ${data.giftName}`);
+                // Streak ended or non-streakable gift => process the gift with final repeat_count
+                // Generate response
+                respondingTo = data.nickname;
+                const statement = `${data.nickname} by name for gifting ${data.repeatCount} ${data.giftName}`
+                responseGenerator(statement, 'gift').then(response => {
+                    // Complete response
+                    speaking = false;
+                });
+            }
+        })
+
+        tiktokLiveConnection.on('follow', (data) => {
+            // Wait for response
+            speaking = true;
+
+            console.log(`${data.uniqueId} (userId:${data.nickname}) followed`);
+            // Generate response
+            respondingTo = data.nickname;
+            const statement = `${data.nickname} by name`
+            responseGenerator(statement, 'follow').then(response => {
+                // Complete response
+                speaking = false;
+            });
+        });
+
+        tiktokLiveConnection.on('emote', data => {
+            // Wait for response
+            speaking = true;
+
+            console.log(`${data.uniqueId} (userId:${data.nickname}) emoted`);
+            // Generate response
+            respondingTo = data.nickname;
+            const statement = `${data.nickname} by name`
+            responseGenerator(statement, 'emote').then(response => {
+                // Complete response
+                speaking = false;
+            });
+        });
+    }
 }
 
-async function textResponseGenerator(statement, liveEvent, callback){
+async function textResponseGenerator(statement, liveEvent, callback) {
     // Sentiment analysis
     var result = sentiment.analyze(statement); // Score: -2, Comparative: -0.666
 
@@ -65,13 +99,11 @@ async function textResponseGenerator(statement, liveEvent, callback){
     // vary response type based on sentiment analysis
     let conversationType = {};
     let conversationTone = "";
-    if (result.score >= 4){
+    if (result.score >= 4) {
         conversationTone = positive[Math.floor(Math.random() * positive.length)];
-    }
-    else if (result.score >= 1 || result.score < 4){
+    } else if (result.score >= 1 || result.score < 4) {
         conversationTone = neutral[Math.floor(Math.random() * neutral.length)];
-    }
-    else if (result.score <= 0){
+    } else if (result.score <= 0) {
         conversationTone = negative[Math.floor(Math.random() * negative.length)];
     }
 
@@ -79,29 +111,29 @@ async function textResponseGenerator(statement, liveEvent, callback){
         conversationTone: conversationTone,
         liveEvent: liveEvent
     };
-    
 
-    await chat(statement, conversationType, function(result){
+
+    await chat(statement, conversationType, function(result) {
         callback(result);
     });
 }
 
-async function responseGenerator(statement, liveEvent){
+async function responseGenerator(statement, liveEvent) {
     // Generate Text response from chatGPT
     let textResponse = "";
-    await textResponseGenerator(statement, liveEvent, function(result){
-        textResponse = result;
+    await textResponseGenerator(statement, liveEvent, function(result) {
+        textResponse = `Hey ${respondingTo}. ${result}`;
     });
-    
+
     // Pass the response to the vocal module
-    vocaliser(textResponse).then(response => {
+    vocaliser(textResponse, respondingTo).then(response => {
         console.log(`RES: ${response}`)
-        player.play(response, function(err){
+        player.play(response, function(err) {
             if (err) {
                 console.log(`ERR: ${err}`)
             }
-          });
+        });
     });
 }
 
-responseGenerator(`How do you work`, 'chat')
+liveStream();
