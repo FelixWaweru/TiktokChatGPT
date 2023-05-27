87646f307754a5e0c403e5e4ba7795fdc3fd8424
unknown <wawerufelix60@gmail.com>
2023-05-27 09:21:04 +0300

    feat: logging

---
+++ logger.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+function log(message) {
+  const timestamp = new Date().toISOString();
+  const logMessage = `${timestamp} - ${message}\n`;
+  console.log(logMessage);
+
+  const logFilePath = path.join(__dirname, `logs/access--${new Date(new Date().getTime() + (3 * 60 * 60 * 1000)).toISOString().slice(0,10)}.log`);
+  fs.appendFile(logFilePath, logMessage, (err) => {
+    if (err) {
+      console.error('Error writing to log file:', err);
+    }
+  });
+}
+
+module.exports = log;
